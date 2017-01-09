1179765ddc0e2e91da5833fed17607b1f389bcfd
NVelikov <nvelikov@abv.bg>
2017-01-09 21:46:16 +0200

    final test

--- task2/script.js
+++ task2/script.js
@@ -2,7 +2,11 @@
 
 // задача 1
 
-var arr = [2, 5, 8, 4, 1, 12];
+var numbers = [2, 5, 8, 4, 1, 12];
+numbers.sort(function(a, b) {
+  return a - b;
+});
+console.log(numbers);
 
 // задача 2
 
@@ -15,4 +19,11 @@ var text = "The quick brown fox jumps over the lazy dog";
 
 
 // задача 5
+
+if (sessionKey myFunc == 'function') {
+
+    myFunc();
+
+}
+
 
